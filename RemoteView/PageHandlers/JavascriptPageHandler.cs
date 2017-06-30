798f6ee0e471a40e02b4a7735fcf00039a062e89
李志强 <love@lizhiqiang.name>
2017-06-30 17:14:51 +0800

    add

--- RemoteView/PageHandlers/JavascriptPageHandler.cs
+++ RemoteView/PageHandlers/JavascriptPageHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Text;
 using System.Windows.Forms;
@@ -21,103 +22,9 @@ namespace RemoteView.PageHandlers
             int screen = GetRequestedScreenDevice(uri, screens);
 
             response.ContentType = "application/javascript";
+            return Encoding.UTF8.GetBytes(File.ReadAllText("js/my.js"));
 
-            return Encoding.UTF8.GetBytes(
-
-                "var http = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject(\"Microsoft.XMLHTTP\");" + Environment.NewLine +
-
-                // cache last mouse down event
-                "var mousedownEvt = null;" + Environment.NewLine +
-
-                // cache mouse move event
-                "var mouseMoveEvt = null;" + Environment.NewLine +
-
-                // listeners for mouse actions
-                "image.addEventListener('contextmenu', function(e){ rightclick(e); });" + Environment.NewLine +
-                "image.addEventListener('mousedown', function(e){ mouseDown(e); });" + Environment.NewLine +
-                "image.addEventListener('mouseup', function(e){ mouseUp(e); });" + Environment.NewLine +
-                "image.addEventListener('load', setReloadTimeout );" + Environment.NewLine +
-                "image.addEventListener('mousemove', function(e){ mouseMove(e); } );" + Environment.NewLine +
-
-                // interval to ask server for a new screen image
-                "setInterval('imageLoader();', 5555);" + Environment.NewLine +
-
-                // interval for mouse move update
-                "setInterval('mouseMoveUpdate();', 333);" + Environment.NewLine +
-
-            // reload image
-            "function mouseMoveUpdate () {" + Environment.NewLine +
-
-                "if (mouseMoveEvt === null) return;" + Environment.NewLine +
-
-                // update mouse position
-                "e = mouseMoveEvt" + Environment.NewLine +
-                "px = e.offsetX ? e.offsetX :e.pageX-document.getElementById(\"image\").offsetLeft;" + Environment.NewLine +
-                "py = e.offsetY ? e.offsetY :e.pageY-document.getElementById(\"image\").offsetTop;" + Environment.NewLine +
-
-                "mouseMoveEvt = null;" + Environment.NewLine +
-                
-                "var request = '/mousemove/" + screen + "/' + py + '/' + px;" + Environment.NewLine +
-                "http.open('GET', request, true);" + Environment.NewLine +
-                "http.send();" + Environment.NewLine +
-
-            "}" + Environment.NewLine +
-                
-            "function setReloadTimeout () {" + Environment.NewLine +
-                // interval to ask server for a new screen image
-                "setTimeout('imageLoader();', 1000);" + Environment.NewLine +
-            "}" + Environment.NewLine +
-
-            // reload image
-            "function imageLoader () {" + Environment.NewLine +
-                " var newImageUrl = '/screen/" + screen + "/' + new Date() / 1;" + Environment.NewLine +
-                " document.getElementById(\"image\").src = newImageUrl; " + Environment.NewLine +
-            "}" + Environment.NewLine +
-
-            "function cancelUiEvts (e) {" + Environment.NewLine +
-                "e.stopPropagation();" + Environment.NewLine +
-                "e.preventDefault();" + Environment.NewLine +
-            "}" + Environment.NewLine +
-
-            "function mouseDown (e) {" + Environment.NewLine +
-                "cancelUiEvts(e);" + Environment.NewLine +
-                "mousedownEvt = e;" + Environment.NewLine +
-            "}" + Environment.NewLine +
-
-            "function mouseUp (e) {" + Environment.NewLine +
-                "cancelUiEvts(e);" + Environment.NewLine +
-
-                "dx = mousedownEvt.offsetX ? mousedownEvt.offsetX :mousedownEvt.pageX-document.getElementById(\"image\").offsetLeft;" + Environment.NewLine +
-                "dy = mousedownEvt.offsetY ? mousedownEvt.offsetY :mousedownEvt.pageY-document.getElementById(\"image\").offsetTop;" + Environment.NewLine +
-
-                "ux = e.offsetX ? e.offsetX :e.pageX-document.getElementById(\"image\").offsetLeft;" + Environment.NewLine +
-                "uy = e.offsetY ? e.offsetY :e.pageY-document.getElementById(\"image\").offsetTop;" + Environment.NewLine +
-
-                "var request = '/leftclick/" + screen + "/' + dy + '/' + dx + '/' + uy + '/' + ux;" + Environment.NewLine +
-                "http.open('GET', request, true);" + Environment.NewLine +
-                "http.send();" + Environment.NewLine +
-
-                "mousedownEvt = null;" + Environment.NewLine +
-
-            "}" + Environment.NewLine +
-
-            "function rightclick(e) { " + Environment.NewLine +
-
-                "cancelUiEvts(e);" + Environment.NewLine +
-
-                "px = e.offsetX ? e.offsetX :e.pageX-document.getElementById(\"image\").offsetLeft;" + Environment.NewLine +
-                "py = e.offsetY ? e.offsetY :e.pageY-document.getElementById(\"image\").offsetTop;" + Environment.NewLine +
-                "var request = '/rightclick/" + screen + "/' + py + '/' + px;" + Environment.NewLine +
-                "http.open('GET', request, true);" + Environment.NewLine +
-                "http.send();" + Environment.NewLine +
-
-            "}" + Environment.NewLine +
-
-            "function mouseMove(e) { " + Environment.NewLine +
-
-                "mouseMoveEvt = e;" + Environment.NewLine +
-
-            "}");
+             
         }
     }
 }
---
+++ RemoteView/js/my.js
@@ -0,0 +1,78 @@
+﻿var http = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+var mousedownEvt = null;
+var mouseMoveEvt = null;
+
+if (image.attachEvent) {
+    //IE处理
+    image.attachEvent('oncontextmenu', function (e) { rightclick(e); });
+    image.attachEvent('onmousedown', function (e) { mouseDown(e); });
+    image.attachEvent('onmouseup', function (e) { mouseUp(e); });
+    image.attachEvent('onload', setReloadTimeout);
+    image.attachEvent('onmousemove', function (e) { mouseMove(e); mouseMoveUpdate() });
+
+} else {
+//非IE处理
+    image.addEventListener('contextmenu', function (e) { rightclick(e); });
+    image.addEventListener('mousedown', function (e) { mouseDown(e); });
+    image.addEventListener('mouseup', function (e) { mouseUp(e); });
+    image.addEventListener('load', setReloadTimeout);
+    image.addEventListener('mousemove', function (e) { mouseMove(e); mouseMoveUpdate() });
+}
+
+setInterval('imageLoader();', 5555);
+setInterval('mouseMoveUpdate();', 333);
+function mouseMoveUpdate() {
+    if (mouseMoveEvt === null) return;
+    e = mouseMoveEvt
+    px = e.offsetX ? e.offsetX : e.pageX - document.getElementById("image").offsetLeft;
+    py = e.offsetY ? e.offsetY : e.pageY - document.getElementById("image").offsetTop;
+    mouseMoveEvt = null;
+    var request = '/mousemove/0/' + py + '/' + px;
+    http.open('GET', request, true);
+    http.send();
+}
+function setReloadTimeout() {
+    setTimeout('imageLoader();', 1000);
+}
+function imageLoader() {
+    var newImageUrl = '/screen/0/' + new Date() / 1;
+    document.getElementById("image").src = newImageUrl;
+}
+function cancelUiEvts(e) {
+    e = window.event || e;
+    if (document.all) {  //只有ie识别
+        e.cancelBubble = true;
+        e.returnValue = false;
+    } else {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+}
+function mouseDown(e) {
+    cancelUiEvts(e);
+    mousedownEvt = e;
+}
+function mouseUp(e) {
+    cancelUiEvts(e);
+    if (mousedownEvt === null) return;
+    dx = mousedownEvt.offsetX ? mousedownEvt.offsetX : mousedownEvt.pageX - document.getElementById("image").offsetLeft;
+    dy = mousedownEvt.offsetY ? mousedownEvt.offsetY : mousedownEvt.pageY - document.getElementById("image").offsetTop;
+    ux = e.offsetX ? e.offsetX : e.pageX - document.getElementById("image").offsetLeft;
+    uy = e.offsetY ? e.offsetY : e.pageY - document.getElementById("image").offsetTop;
+    var request = '/leftclick/0/' + dy + '/' + dx + '/' + uy + '/' + ux;
+    http.open('GET', request, true);
+    http.send();
+    mousedownEvt = null;
+}
+function rightclick(e) {
+    cancelUiEvts(e);
+    if (e === null) return;
+    px = e.offsetX ? e.offsetX : e.pageX - document.getElementById("image").offsetLeft;
+    py = e.offsetY ? e.offsetY : e.pageY - document.getElementById("image").offsetTop;
+    var request = '/rightclick/0/' + py + '/' + px;
+    http.open('GET', request, true);
+    http.send();
+}
+function mouseMove(e) {
+    mouseMoveEvt = e;
+}
